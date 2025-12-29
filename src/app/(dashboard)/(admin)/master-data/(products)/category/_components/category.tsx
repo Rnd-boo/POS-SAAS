bbcc60bd5c161841fd276142453879cae0f55d66
Randy <nelayanca01@gmail.com>
2025-12-29 16:58:02 +0700

    refactor: change data table into tanstack table

--- src/app/(dashboard)/(admin)/master-data/(products)/category/_components/category.tsx
+++ src/app/(dashboard)/(admin)/master-data/(products)/category/_components/category.tsx
@@ -10,7 +10,14 @@ import useDataTable from "@/hooks/use-data-table";
 import { createClient } from "@/lib/supabase/client";
 import { cn } from "@/lib/utils";
 import { useQuery } from "@tanstack/react-query";
-import { Pen, Pencil, Trash2 } from "lucide-react";
+import {
+  ArrowUpDown,
+  MoreHorizontal,
+  Pen,
+  Pencil,
+  SearchIcon,
+  Trash2,
+} from "lucide-react";
 import { useMemo, useState } from "react";
 import { toast } from "sonner";
 import DialogCreateCategory from "./dialog-create-category";
@@ -18,6 +25,23 @@ import DialogUpdateCategory from "./dialog-update-category";
 import { Category } from "@/validations/category-validation";
 import DialogDeleteCategory from "./dialog-delete-category";
 import { useAuthStore } from "@/stores/auth-store";
+import { DataTableDemo } from "@/components/common/tanstack-table";
+import { ColumnDef } from "@tanstack/react-table";
+import { Checkbox } from "@/components/ui/checkbox";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupButton,
+  InputGroupInput,
+} from "@/components/ui/input-group";
 
 export default function CategoryManagement() {
   const supabase = createClient();
@@ -29,6 +53,8 @@ export default function CategoryManagement() {
     handleChangePage,
     currentSearch,
     handleChangeSearch,
+    setTotalData,
+    totalData,
   } = useDataTable();
 
   const {
@@ -52,11 +78,12 @@ export default function CategoryManagement() {
         .order("name")
         .ilike("name", `%${currentSearch}%`);
 
+      setTotalData(result.count ?? 0);
+
       if (result.error)
         toast.error("Get Category Data Failed", {
           description: result.error.message,
         });
-
       return result;
     },
     enabled: !!currentId,
@@ -123,24 +150,133 @@ export default function CategoryManagement() {
       : 0;
   }, [categories]);
 
+  // tanstack section
+  const data: Category[] = categories?.data || [];
+  const columns: ColumnDef<Category>[] = [
+    {
+      accessorKey: "name",
+      header: ({ column }) => {
+        return (
+          <div
+            className="flex gap-2 font-medium items-center"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            Category Name
+            <ArrowUpDown className=" size-4" />
+          </div>
+        );
+      },
+      cell: ({ row }) => (
+        <div className="capitalize">{row.getValue("name")}</div>
+      ),
+      enableHiding: false,
+    },
+    {
+      accessorKey: "description",
+      enableHiding: false,
+      header: () => <div>Description</div>,
+      cell: ({ row }) => (
+        <div className="truncate max-w-xs">{row.getValue("description")}</div>
+      ),
+    },
+    {
+      accessorKey: "status",
+      enableHiding: false,
+      header: () => <div>Status</div>,
+      cell: ({ row }) => {
+        const status = row.getValue("status");
+
+        return (
+          <div
+            className={cn(
+              "px-2 py-1 rounded-full text-white w-fit",
+              status ? "bg-green-600" : "bg-red-500"
+            )}
+          >
+            {status ? "Active" : "Inactive"}
+          </div>
+        );
+      },
+    },
+    {
+      id: "actions",
+      enableHiding: false,
+      header: () => <div className="flex justify-center">Actions</div>,
+      cell: ({ row }) => {
+        return (
+          <DropdownAction
+            menu={[
+              {
+                label: (
+                  <span className="flex items-center gap-2">
+                    <Pencil />
+                    Edit
+                  </span>
+                ),
+                action: () => {
+                  setSelectedAction({
+                    data: row.original,
+                    type: "update",
+                  });
+                },
+              },
+              {
+                label: (
+                  <span className="flex items-center gap-2">
+                    <Trash2 className="text-red-400" />
+                    Delete
+                  </span>
+                ),
+                variant: "destructive",
+                action: () => {
+                  setSelectedAction({
+                    data: row.original,
+                    type: "delete",
+                  });
+                },
+              },
+            ]}
+          />
+        );
+      },
+    },
+  ];
+  //  <div className="flex items-center py-4">
+  //         <Input
+  //           placeholder={`Search by ${title}`}
+  //           onChange={(e) => handleChangeSearch(e.target.value)}
+  //           className="max-w-sm"
+  //         />
+  // <Dialog>
+  //         <DialogTrigger asChild>
+  //           <Button variant="outline">Create</Button>
+  //         </DialogTrigger>
+  //         <DialogCreateCategory refetch={refetch} />
+  //       </Dialog>
+  //       </div>
   return (
     <div className="w-full">
       <div className="flex flex-col lg:flex-row mb-4 gap-2 justify-between w-full">
-        <h1 className="text-xl font-semibold">Category Management</h1>
-        <div className="flex gap-2">
-          <Input
+        <h1 className="text-2xl px-2 font-semibold">Category Management</h1>
+      </div>
+      <div className="mb-2 flex justify-between">
+        <InputGroup className="max-w-sm">
+          <InputGroupInput
             placeholder="Search by category name"
             onChange={(e) => handleChangeSearch(e.target.value)}
           />
-          <Dialog>
-            <DialogTrigger asChild>
-              <Button variant="outline">Create</Button>
-            </DialogTrigger>
-            <DialogCreateCategory refetch={refetch} />
-          </Dialog>
-        </div>
+          <InputGroupAddon>
+            <SearchIcon />
+          </InputGroupAddon>
+        </InputGroup>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="outline">Create</Button>
+          </DialogTrigger>
+          <DialogCreateCategory refetch={refetch} />
+        </Dialog>
       </div>
-      <DataTable
+      {/* <DataTable
         header={HEADER_TABLE_CATEGORY}
         isLoading={isLoading}
         data={filteredData}
@@ -149,6 +285,15 @@ export default function CategoryManagement() {
         currentLimit={currentLimit}
         onChangePage={handleChangePage}
         onChangeLimit={handleChangeLimit}
+      /> */}
+      <DataTableDemo
+        data={data}
+        columns={columns}
+        totalPages={totalPages}
+        currentPage={currentPage}
+        onChangePage={handleChangePage}
+        totalData={totalData}
+        refetch={refetch}
       />
       <DialogUpdateCategory
         open={selectedAction !== null && selectedAction.type === "update"}
