086f0f3c4b94d6b27e7be58bd2c643048972be28
Randy <rand4035@gmail.com>
2026-01-10 10:36:17 +0700

    refactor: data-table menu category

--- src/app/(dashboard)/(admin)/master-data/(pos)/menu-category/_components/menu-category.tsx
+++ src/app/(dashboard)/(admin)/master-data/(pos)/menu-category/_components/menu-category.tsx
@@ -1,36 +1,36 @@
 "use client";
 
-import DataTable from "@/components/common/data-table";
-import { Button } from "@/components/ui/button";
-import { Dialog, DialogTrigger } from "@/components/ui/dialog";
-import { Input } from "@/components/ui/input";
 import useDataTable from "@/hooks/use-data-table";
 import { createClient } from "@/lib/supabase/client";
 import { cn } from "@/lib/utils";
 import { useAuthStore } from "@/stores/auth-store";
 import { useBrandStore } from "@/stores/brand-store";
 import { useQuery } from "@tanstack/react-query";
-import { Pencil, Trash2 } from "lucide-react";
+import { ArrowDown, ArrowUp, Pencil, Trash2 } from "lucide-react";
 import { ReactNode, useMemo, useState } from "react";
 import { toast } from "sonner";
 import type { MenuCategory } from "@/validations/pos/menu-category.validation";
-import { HEADER_TABLE_MENU_CATEGORY } from "@/constants/pos/menu-category";
 import DialogCreateMenuCategory from "./dialog-create-menu-category";
 import DialogDeleteMenuCategory from "./dialog-delete-menu-category";
 import DialogUpdateMenuCategory from "./dialog-update-menu-category";
+import { DataTable } from "@/components/common/tanstack-table";
+import { ColumnDef, SortingState } from "@tanstack/react-table";
+import DropdownAction from "@/components/common/dropdown-action";
+import PageHeader from "@/components/common/page-header";
 
 export default function MenuCategory() {
   const supabase = createClient();
   const currentBrandId = useBrandStore((s) => s.currentBrandId);
   const currentId = useAuthStore((state) => state.profile?.clients);
+  const [sorting, setSorting] = useState<SortingState>([]);
 
   const {
-    currentLimit,
     currentPage,
-    handleChangeLimit,
     handleChangePage,
     currentSearch,
     handleChangeSearch,
+    totalData,
+    setTotalData,
   } = useDataTable();
 
   const {
@@ -41,22 +41,31 @@ export default function MenuCategory() {
     queryKey: [
       "menu_category",
       currentPage,
-      currentLimit,
       currentSearch,
       currentBrandId,
       currentId,
+      sorting,
     ],
     queryFn: async () => {
-      const result = await supabase
+      let query = supabase
         .from("menu_category")
         .select("id,name,status,brand_id", {
           count: "exact",
         })
         .eq("clients_id", currentId)
         .eq("brand_id", currentBrandId)
-        .order("name")
         .ilike("name", `%${currentSearch}%`);
 
+      const sort = sorting[0];
+
+      if (sort) {
+        query.order(sort.id, { ascending: sort.desc ? false : true });
+      } else {
+        query.order("created_at", { ascending: false });
+      }
+
+      const result = await query;
+      setTotalData(result.count || 0);
       if (result.error) {
         toast.error("Get Menu Category data Failed");
       }
@@ -85,83 +94,110 @@ export default function MenuCategory() {
     }
   };
 
-  const filteredData = useMemo(() => {
-    return (menuCategories?.data || []).map((menuCategory, index) => {
-      return [
-        currentLimit * (currentPage - 1) + index + 1,
-        menuCategory.name,
-        <div
-          className={cn(
-            "px-2 py-1 rounded-full text-white w-fit",
-            menuCategory.status ? "bg-green-600" : "bg-red-500"
-          )}
-        >
-          {menuCategory.status ? "Active" : "Inactive"}
-        </div>,
-        <div className="flex  gap-1">
-          <Button
-            variant="ghost"
-            size="icon"
-            className="cursor-pointer size-6 hover:text-muted-foreground hover:!bg-muted-foreground/40"
-            onClick={() => {
-              setSelectedAction({
-                data: menuCategory,
-                type: "update",
-              });
-            }}
+  const data: MenuCategory[] = menuCategories?.data || [];
+  const columns: ColumnDef<MenuCategory>[] = [
+    {
+      accessorKey: "name",
+      enableHiding: false,
+      header: ({ column }) => {
+        const sorted = column.getIsSorted();
+        return (
+          <div
+            className="flex gap-2 font-medium items-center"
+            onClick={() => column.toggleSorting(undefined, true)}
           >
-            <Pencil />
-          </Button>
-          <Button
-            variant="ghost"
-            size="icon"
-            className="cursor-pointer size-6 text-destructive hover:text-muted-foreground hover:!bg-muted-foreground/40"
-            onClick={() => {
-              setSelectedAction({
-                data: menuCategory,
-                type: "delete",
-              });
-            }}
-          >
-            <Trash2 />
-          </Button>
-        </div>,
-      ];
-    });
-  }, [menuCategories]);
+            Category Name
+            {sorted === "asc" && <ArrowUp className="size-3" />}
+            {sorted === "desc" && <ArrowDown className="size-3" />}
+          </div>
+        );
+      },
+      cell: ({ row }) => <div>{row.getValue("name")}</div>,
+    },
+    {
+      accessorKey: "status",
+      enableHiding: false,
+      header: () => <div>Status</div>,
+      cell: ({ row }) => {
+        const status = row.getValue("status");
 
+        return (
+          <div
+            className={cn(
+              "px-2 py-1 rounded-full text-white w-fit",
+              status ? "bg-green-600" : "bg-red-500"
+            )}
+          >
+            {status ? "Active" : "Inactive"}
+          </div>
+        );
+      },
+    },
+    {
+      id: "actions",
+      enableHiding: false,
+      header: () => <div className="flex justify-center">Actions</div>,
+      cell: ({ row }) => {
+        return (
+          <DropdownAction
+            menu={[
+              {
+                label: (
+                  <span className="flex items-center gap-2">
+                    <Pencil />
+                    Edit
+                  </span>
+                ),
+                action: () => {
+                  setSelectedAction({
+                    data: row.original,
+                    type: "update",
+                  });
+                },
+              },
+              {
+                label: (
+                  <span className="flex items-center gap-2">
+                    <Trash2 className="text-red-400" />
+                    Delete
+                  </span>
+                ),
+                variant: "destructive",
+                action: () => {
+                  setSelectedAction({
+                    data: row.original,
+                    type: "delete",
+                  });
+                },
+              },
+            ]}
+          />
+        );
+      },
+    },
+  ];
   const totalPages = useMemo(() => {
     return menuCategories && menuCategories.count !== null
-      ? Math.ceil(menuCategories.count / currentLimit)
+      ? Math.ceil(menuCategories.count / 10)
       : 0;
   }, [menuCategories]);
   return (
     <div className="w-full">
-      <div className="flex flex-col lg:flex-row mb-4 gap-2 justify-between w-full">
-        <h1 className="text-xl font-semibold">Menu Category Management</h1>
-        <div className="flex gap-2">
-          <Input
-            placeholder="Search by Menu Category"
-            onChange={(e) => handleChangeSearch(e.target.value)}
-          />
-          <Dialog>
-            <DialogTrigger asChild>
-              <Button variant="outline">Create</Button>
-            </DialogTrigger>
-            <DialogCreateMenuCategory refetch={refetch} />
-          </Dialog>
-        </div>
-      </div>
+      <PageHeader
+        handleChangeSearch={handleChangeSearch}
+        title="category"
+        DialogCreateComponent={<DialogCreateMenuCategory refetch={refetch} />}
+      />
       <DataTable
-        handleView={handleViewTable}
-        header={HEADER_TABLE_MENU_CATEGORY}
-        isLoading={isLoading}
-        data={filteredData}
+        data={data}
+        columns={columns}
         totalPages={totalPages}
         currentPage={currentPage}
-        currentLimit={currentLimit}
         onChangePage={handleChangePage}
-        onChangeLimit={handleChangeLimit}
+        totalData={totalData}
+        refetch={refetch}
+        sorting={sorting}
+        onSortingChange={setSorting}
       />
       <DialogUpdateMenuCategory
         open={selectedAction !== null && selectedAction.type === "update"}
