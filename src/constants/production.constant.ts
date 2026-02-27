fffbaf14ee9af38e4772dfceaed0c560b2fa2337
Randy <rand4035@gmail.com>
2026-02-28 02:44:23 +0700

    refactor: order to orders page

--- src/constants/production.constant.ts
+++ src/constants/production.constant.ts
@@ -3,35 +3,33 @@ import {
   ClipboardCopy,
   ClipboardPlus,
   ClipboardX,
- 
 } from "lucide-react";
 
 export const PRODUCTION_CARD = [
-      {
-        title: "Production Order",
-        icon: ClipboardPlus,
-        url: "/production/order",
-        description:
-          "Plan and track the production planning (Demand forecasting).",
-      },
-      {
-        title: "Production Process",
-        icon: ClipboardCopy,
-        url: "/production/process",
-        description:
-          "Execute and monitor production activities.",
-      },
-      {
-        title: "Production Confirmation",
-        url: "/production/confirmation",
-        icon: ClipboardCheck,
-        description: "Confirm completed production results for finished goods and consumed materials.",
-      },
-      {
-        title: "Production Close",
-        url: "/production/close",
-        icon: ClipboardX,
-        description:
-          "Finalize the production order by closing all remaining qty and locking further changes.",
-      },
-    ]
+  {
+    title: "Production Orders",
+    icon: ClipboardPlus,
+    url: "/production/orders",
+    description: "Plan and track the production planning (Demand forecasting).",
+  },
+  {
+    title: "Production Process",
+    icon: ClipboardCopy,
+    url: "/production/process",
+    description: "Execute and monitor production activities.",
+  },
+  {
+    title: "Production Confirmation",
+    url: "/production/confirmation",
+    icon: ClipboardCheck,
+    description:
+      "Confirm completed production results for finished goods and consumed materials.",
+  },
+  {
+    title: "Production Close",
+    url: "/production/close",
+    icon: ClipboardX,
+    description:
+      "Finalize the production order by closing all remaining qty and locking further changes.",
+  },
+];
